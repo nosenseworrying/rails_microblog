eda91d8f404dbb82609671b7a97d25fcb59a5944
Matt Augustine <nosenseworrying@gmail.com>
2015-08-31 14:07:16 -0700

    Finish user edit, update, index, destroy and exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,13 +5,37 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
-  test "layout links" do
+  def setup
+    @user = users(:matt)
+  end
+
+  test "layout links for non-logged in user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+  end
+
+  test "layout links for logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path    
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 
   test "visit signup" do
